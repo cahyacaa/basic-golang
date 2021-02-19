9b89cc69eb924a6e89b2a3e5652fe2dac657bec5
CahyaN <cahyanug3@gmail.com>
2021-02-19 10:02:45 +0700

    conditional and loop

---
+++ condition.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main(){
+	point:=10
+	if point >=9{
+		fmt.Println("value greater than initial value, your value is",point)
+	}
+//repeating
+	i:=1
+	for i := 0; i < 5; i++ {
+		fmt.Println("Number", i)
+	}
+
+	for i < 5 {
+		fmt.Println("Angka", i)
+		i++
+	}
+	for i := 0; i < 5; i++ {
+		for j := i; j < 5; j++ {
+			fmt.Print(j)
+		}
+	}
+
+}
