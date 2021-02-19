fb1a8d22018bcba8b4acc19ddccc1c90b382f8a5
CahyaN <cahyanug3@gmail.com>
2021-02-19 18:05:34 +0700

    nested func

--- struct.go
+++ struct.go
@@ -7,14 +7,19 @@ import (
 type student struct {
 	name string 
 	grade int
+	languages
+}
+
+type languages struct{
+	intermediate []string
 }
 
 func main(){
-	var s1 student 
+	var s1= student{} 
 	s1.name = "Ryu Hye-Young"
 	s1.grade = 10
-
-
+	s1.intermediate = []string{"lol","lal"}
 	fmt.Println("name :", s1.name)
 	fmt.Println("grade :", s1.grade)
+	fmt.Println("languages :", s1.languages.intermediate)
 }
