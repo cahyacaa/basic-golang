ca6d059dfa0a0ae8edea36f6c9d43b2422384d59
CahyaN <cahyanug3@gmail.com>
2021-02-22 09:14:33 +0700

    add

--- method.go
+++ method.go
@@ -18,9 +18,22 @@ func (s student) getNameAt(i int) string {
     return strings.Split(s.name, " ")[i-1]
 }
 
+type drakor struct{
+	Nama string
+	Rating string
+	Genre string
+}
+
+func(d drakor) detailDrama(){
+	fmt.Printf("Judul Drama :%s, Rating : %s, Genre : %s \n", d.Nama, d.Rating, d.Genre)
+}
+
 func main(){
 	var s1 = student{"Cahya Nugroho", 24}
 	s1.sayHello()
+
+	drama1:= drakor{"Reply 1988", "17.84%" ,"Comedy, Romance"}
+	drama1.detailDrama()
 
 	var name = s1.getNameAt(2)
     fmt.Println("nama panggilan :", name)
