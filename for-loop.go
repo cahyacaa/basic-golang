2863ea6d4d3b612b55c10467e851e55e00b0dcc1
= <=>
2022-05-30 10:27:34 +0700

    add object struct

--- for-loop.go
+++ for-loop.go
@@ -2,9 +2,32 @@ package main
 
 import "fmt"
 
+type Users struct {
+	name string
+	age  int32
+}
+
 func main() {
 	sekumpulan := [...]int{1, 2, 3, 4, 4, 3, 3}
 	for index, number := range sekumpulan {
 		fmt.Printf("%d adalah index %d \n", number, index)
+	}
+
+	kvs := map[string]string{"a": "apple", "b": "banana"}
+	akvs := []Users{{
+		name: "cahya",
+		age:  12,
+	},
+		{
+			name: "agil",
+			age:  19,
+		},
+	}
+	for k, v := range kvs {
+		fmt.Printf("%s -> %s\n", k, v)
+	}
+
+	for k, v := range akvs {
+		fmt.Println(k, v)
 	}
 }
