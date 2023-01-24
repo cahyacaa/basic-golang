6ac146b9e39e22caa76f08a6c2a8ff35ed41a9c0
cahyacaa <cahyanug3@gmail.com>
2023-01-24 11:31:07 +0700

    update mutex

--- mutex.go
+++ mutex.go
@@ -1,45 +1,41 @@
 package main
 
 import (
-    "fmt"
-    "runtime"
-    "sync"
+	"fmt"
+	"runtime"
+	"sync"
 )
 
 type counter struct {
-    val int
-    sync.Mutex
+	val int
+	*sync.Mutex
 }
 
 func (c *counter) Add(x int) {
-    c.Lock()
-    c.val++
-    c.Unlock()
+	c.Lock()
+	c.val++
+	c.Unlock()
 }
 
 func (c *counter) Value() (x int) {
-    return c.val
+	return c.val
 }
 
 func main() {
-    runtime.GOMAXPROCS(2)
-    m := make(map[int]int)
-    var wg sync.WaitGroup
-    // var meter counter
-    var mtx sync.Mutex
-    wg.Add(10)
-    for i := 0; i < 10; i++ {
-      
+	runtime.GOMAXPROCS(2)
+	var wg sync.WaitGroup
 
-        go func() {
-                mtx.Lock()
-                m[i] += i
-                mtx.Unlock()
-                fmt.Println(m[i])
-            wg.Done()
-        }()
-    }
+	count := counter{
+		val: 1,
+	}
+	wg.Add(10)
+	for i := 0; i < 10; i++ {
+		go func() {
+			defer wg.Done()
+			count.Add(count.val)
+		}()
+	}
 
-    wg.Wait()
-    println(len(m))
+	wg.Wait()
+	fmt.Println(count.Value())
 }
